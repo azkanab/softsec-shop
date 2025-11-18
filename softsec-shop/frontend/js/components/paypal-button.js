27cb95c880d2cf318e52e32de435a86f56589d30
Azka Nabilah <azkanabilah@gmail.com>
2025-11-19 00:20:25 +0100

    Fix indentation

--- softsec-shop/frontend/js/components/paypal-button.js
+++ softsec-shop/frontend/js/components/paypal-button.js
@@ -44,44 +44,44 @@ window.paypal
 
     // Task 3.5 Step 2: Fetch from our API in file order/views.py to call paypal_notify function to finalize/capture payment in Paypal
     async onApprove(data, actions) {
-    try {
-        const response = await fetch(`/orders/paypal/gateway/${data.orderID}/`, {
-            method: "POST",
-            headers: {
-                "Content-Type": "application/json",
-                "X-CSRFToken": csrfToken
-            },
-        });
+        try {
+            const response = await fetch(`/orders/paypal/gateway/${data.orderID}/`, {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json",
+                    "X-CSRFToken": csrfToken
+                },
+            });
 
-        const orderData = await response.json();
-        // Three cases to handle:
-        //   (1) Recoverable INSTRUMENT_DECLINED -> call actions.restart()
-        //   (2) Other non-recoverable errors -> Show a failure message
-        //   (3) Successful transaction -> Show confirmation or thank you message
+            const orderData = await response.json();
+            // Three cases to handle:
+            //   (1) Recoverable INSTRUMENT_DECLINED -> call actions.restart()
+            //   (2) Other non-recoverable errors -> Show a failure message
+            //   (3) Successful transaction -> Show confirmation or thank you message
 
-        const errorDetail = orderData?.details?.[0];
+            const errorDetail = orderData?.details?.[0];
 
-        if (errorDetail?.issue === "INSTRUMENT_DECLINED") {
-            // (1) Recoverable INSTRUMENT_DECLINED -> call actions.restart()
-            // recoverable state, per
-            // https://developer.paypal.com/docs/checkout/standard/customize/handle-funding-failures/
-            return actions.restart();
-        } else if (errorDetail) {
-            // (2) Other non-recoverable errors -> Show a failure message
-            throw new Error(`${errorDetail.description} (${orderData.debug_id})`);
-        } else if (!orderData.purchase_units) {
-            throw new Error(JSON.stringify(orderData));
-        } else {
-            // (3) Successful transaction -> Show confirmation or thank you message
-            // Or go to another URL:  actions.redirect('thank_you.html');
-            actions.redirect(orderData.redirect_url)
+            if (errorDetail?.issue === "INSTRUMENT_DECLINED") {
+                // (1) Recoverable INSTRUMENT_DECLINED -> call actions.restart()
+                // recoverable state, per
+                // https://developer.paypal.com/docs/checkout/standard/customize/handle-funding-failures/
+                return actions.restart();
+            } else if (errorDetail) {
+                // (2) Other non-recoverable errors -> Show a failure message
+                throw new Error(`${errorDetail.description} (${orderData.debug_id})`);
+            } else if (!orderData.purchase_units) {
+                throw new Error(JSON.stringify(orderData));
+            } else {
+                // (3) Successful transaction -> Show confirmation or thank you message
+                // Or go to another URL:  actions.redirect('thank_you.html');
+                actions.redirect(orderData.redirect_url)
+            }
+        } catch (error) {
+            console.error(error);
+            errorMessage(
+            `Sorry, your transaction could not be processed...<br><br>${error}`
+            );
         }
-    } catch (error) {
-        console.error(error);
-        errorMessage(
-        `Sorry, your transaction could not be processed...<br><br>${error}`
-        );
-    }
     },
 })
 .render("#paypal-button-container");
